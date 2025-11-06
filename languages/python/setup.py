c881cc7cb9eeb59d42edde57acdb78f3453fb7e8
Matthew Brumagin <mcbrumagin@gmail.com>
2025-11-06 14:11:38 -0500

    Make a proper module

--- languages/python/setup.py
+++ languages/python/setup.py
@@ -9,27 +9,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="microjs-client",
+    name="microjs",
     version="1.0.0",
     author="Micro-JS Team",
     description="Python client library for the Micro-JS microservices framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/micro-js",
-    packages=find_packages(),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
+    packages=['microjs'],
     python_requires=">=3.7",
     install_requires=[
         "requests>=2.28.0",
